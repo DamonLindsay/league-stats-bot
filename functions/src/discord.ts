6a58b49039da25935c76f9e95175ec3d8207ae39
Damon Lindsay <damon.lindsay@my.jcu.edu.au>
2026-07-13 17:31:36 +1000

    feat: add postImageToDiscord for image attachment uploads

--- functions/src/discord.ts
+++ functions/src/discord.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
+import FormData from "form-data"
 
 /**
  * Posts a message to a Discord channel via webhook, appearing as "Orianna"
  */
-
 export async function postToDiscord(
     webhookUrl: string,
     message: string
@@ -11,5 +11,30 @@ export async function postToDiscord(
     await axios.post(webhookUrl, {
         username: "Orianna",
         content: message,
+    });
+}
+
+/**
+ * Posts a PNG image to a Discord channel via webhook, appearing as "Orianna".
+ */
+export async function postImageToDiscord(
+    webhookUrl: string,
+    imageBuffer: Buffer,
+    filename: string
+): Promise<void> {
+    const form = new FormData();
+
+    form.append(
+        "payload_json",
+        JSON.stringify({ username: "Orianna"})
+    );
+
+    form.append("file", imageBuffer, {
+        filename,
+        contentType: "image/png",
+    });
+
+    await axios.post(webhookUrl, form, {
+        headers: form.getHeaders(),
     });
 }
